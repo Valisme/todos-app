061de0353c65b6c73ff45cfc00ebead27ff79d2f
Valmero Ahmadtofa <valmero@LAPTOP-TPIVCUSD>
2023-03-19 15:37:34 +0700

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+    import "../app.css";
+  </script>
+  <nav class="bg-blue-500 w-full h-20 flex flex-row items-center p-4">
+    <li class="text-white flex flex-row gap-6 text-3xl font-mono">
+        <ul><a href="/tasks">Tasks</a></ul>
+        <ul><a href="/class">Class</a></ul>
+        <ul><a href="/forum">Forum</a></ul>
+    </li>
+    <div class="ml-auto w-14 h-14 bg-white rounded-full">
+    </div>
+</nav>
+  <slot />
+
+<style lang="postcss">
+    :global(html) {
+        height: 100vh;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let data;
+
+	let { tasks } = data;
+    console.log(tasks)
+</script>
+
+<div class="flex flex-col p-16 gap-4">
+	<p class="text-6xl font-mono">Hello, Valmero</p>
+	<p class="text-5xl font-semibold">Task</p>
+	<div class="bg-blue-400 h-[512px] w-full rounded-3xl border-4 border-blue-500">
+		{#each tasks as task}
+			<p class="text-2xl">{task.title}</p>
+			<p class="text-2xl">{task.description}</p>
+		{/each}
+	</div>
+	<br />
+	<p class="text-5xl font-semibold">Class</p>
+	<div class="bg-blue-400 h-[512px] w-full rounded-3xl border-4 border-blue-500" />
+</div>
