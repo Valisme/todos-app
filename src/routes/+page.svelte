078139ca5af81256055c1daa002675b50f2f862b
Valmero Ahmadtofa <valmero@LAPTOP-TPIVCUSD>
2023-03-20 00:29:45 +0700

    Making Basic UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,46 @@
 <script>
-	export let data;
-
-	let { tasks } = data;
-    console.log(tasks)
+	let title;
+	let description;
+	let err;
+	function print() {
+		if(!title && !description){
+			err = "Can't Save Empty Value!"
+		}
+		else if(!title){
+			err = "Can't Save Empty Title!"
+		}
+		else if(!description){
+			err = "Can't Save Empty Description!"
+		}
+		else {
+		console.log(title)
+		console.log(description)
+		err=null;
+	    }
+	}
 </script>
 
-<div class="flex flex-col p-16 gap-4">
-	<p class="text-6xl font-mono">Hello, Valmero</p>
-	<p class="text-5xl font-semibold">Task</p>
-	<div class="bg-blue-400 h-[512px] w-full rounded-3xl border-4 border-blue-500">
-		{#each tasks as task}
-			<p class="text-2xl">{task.title}</p>
-			<p class="text-2xl">{task.description}</p>
-		{/each}
+<div class="flex flex-col gap-5 p-10 font-mono">
+	<p>Make some Tasks</p>
+	<label for="title">Title</label>
+	<input id="title" type="text" class="w-60 border-4 transition-all ease-linear focus:w-64 focus:solid-shadow border-yellow-300 outline-none" bind:value={title} />
+	<label for="description">Description</label>
+	<input id="description" type="text" class="w-60 border-4 transition-all ease-linear focus:w-64 focus:solid-shadow border-yellow-300 outline-none" bind:value={description} />
+	<button class="bg-blue-600 text-white w-20 transition-all ease-out duration-300 hover:text-black hover:solid-shadow active:bg-blue-800" on:click={() => print()}> Add Task</button>
+	{#if err}
+		<alert class="text-red-600 font-bold">
+			{err}
+		</alert>
+	{/if}
+	<div class="grid grid-flow-row mt-2">
+		<div class="h-80 w-80 p-4 flex flex-col gap-4 text-white bg-blue-500 transition-all ease-in hover:text-black hover:bg-blue-600 hover:solid-shadow-xl hidden-scroll overflow-hidden hover:overflow-y-scroll hover:overflow-x-hidden">
+			<p class="text-xl">
+				This is Title!
+			</p>
+			<p class="text-lg leading-relaxed">
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident doloribus, excepturi delectus facere, voluptatum esse perferendis pariatur ex cum, nobis perspiciatis. Rem, ratione quibusdam! Earum temporibus dolores nam praesentium reprehenderit!
+			</p>
+		</div>
 	</div>
-	<br />
-	<p class="text-5xl font-semibold">Class</p>
-	<div class="bg-blue-400 h-[512px] w-full rounded-3xl border-4 border-blue-500" />
 </div>
+
