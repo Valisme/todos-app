a6f9cfac36598acedcca2094959b5e0772dedbd0
Valmero Ahmadtofa <valmero@LAPTOP-TPIVCUSD>
2023-03-31 15:22:27 +0700

    Remove import type line

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,13 +1,11 @@
 import PocketBase from "pocketbase"
 import {env} from "$env/dynamic/private"
 
-/** @type {import('@sveltejs/kit').Handle} */
 
 export const handle = async ({event,resolve})=>{
+
     event.locals.pb = new PocketBase(env.POCKETBASE_URL)
     event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || "")
-
-    
     if (event.locals.pb.authStore.isValid){
         event.locals.user = event.locals.pb.authStore.model
     }
