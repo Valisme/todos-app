7520069787e11ef22f866ac29e04867bb0f39269
Valmero Ahmadtofa <valmero@LAPTOP-TPIVCUSD>
2023-03-31 16:40:12 +0700

    Add logout function

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,9 +11,16 @@
 		<button class="bg-blue-500 text-white hover:text-black hover:bg-blue-600 active:scale-90 active:bg-blue-700 hover:solid-shadow transition-all ease-out duration-300 delay-75 px-4 py-2 " on:click={()=>goto("/register")}>Register</button>
 	</div>
 {:else}
-	<div class="flex flex-row-reverse mr-20 text-lg pt-5 font-mono">
+	<div class="flex flex-row-reverse gap-5 items-center mr-20 text-lg pt-5 font-mono">
+		<form action="/logout" method="post">
+			<button type="submit" class="bg-blue-500 text-white hover:text-black hover:bg-blue-600 active:scale-90 active:bg-blue-700 hover:solid-shadow transition-all ease-out duration-300 delay-75 px-2 py-1 ">
+				Logout
+			</button>
+		</form>
         <p>Welcome to Todos App, {data.name}</p>
+		
 	</div>
+	
 {/if}
 
 <slot />
