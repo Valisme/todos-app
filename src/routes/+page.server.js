5785617efae1175461d679c789c68fad89f4d82d
Valmero Ahmadtofa <valmero@LAPTOP-TPIVCUSD>
2023-03-31 15:52:30 +0700

    Add add & remove tasks to databases

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,8 +1,49 @@
-import { redirect } from '@sveltejs/kit';
+import { fail, redirect } from '@sveltejs/kit';
+import { toJSON } from '../lib/convertPojo';
 
-/** @type {import('./$types').PageServerLoad} */
-export async function load({locals}) {
-	if(!locals.pb.authStore.isValid){
-		throw redirect('303','/login')
+export async function load({ locals }) {
+	if (!locals.pb.authStore.isValid) {
+		throw redirect('303', '/login');
+	}
+
+	if (locals.user) {
+		try {
+			const records = await locals.pb.collection('users').getOne(locals.user.id, {
+				expand: 'tasks'
+			});
+			const notes = toJSON(records.expand.tasks)
+			return { notes };
+		} catch (_) {
+			return null;
+		}
 	}
 }
+
+export const actions = {
+	add: async ({ locals, request }) => {
+		const req = await request.formData();
+		const { title, description } = Object.fromEntries(req);
+
+		if (title === '' && description === '') {
+			return fail(400, { err: "Can't Submit Empty Value!" });
+		} else if (title === '') {
+			return fail(400, { err: "Can't Submit Empty Title!" });
+		} else {
+			const task = await locals.pb.collection('tasks').create({
+				title,
+				description,
+				user: locals.user.id
+			});
+			const { tasks } = await locals.pb.collection('users').getOne(locals.user.id);
+			await locals.pb.collection('users').update(locals.user.id, { tasks: [task.id, ...tasks] });
+			return toJSON(task)
+		}
+	},
+	delete:async ({request,locals}) =>{
+		const req = await request.formData()
+		const {id} = Object.fromEntries(req)
+		await locals.pb.collection('tasks').delete(id)
+		
+		return {id}
+	}
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,81 +1,76 @@
 <script>
+	export let data;
+	export let form;
+	import { enhance } from '$app/forms';
+	let todos= data.notes ?? [];
 
+	const formSubmit =  ({data,cancel})=>{
+		return async ({update,result})=>{
+			todos = [result.data,...todos]
+			
+		 	await update()
+		}
+	}
 	
-
-	let inputTitle = "";
-	let inputDescription = "";
-	let err;
-	let uuid=0;
-
-	let todos = [
-		{ id: uuid++, title: 'This is Title', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, molestias doloribus eius nesciunt, aspernatur a, perferendis suscipit laborum distinctio ipsam assumenda ipsa esse fugiat quam officia omnis saepe laboriosam autem.' },
-		{ id: uuid++, title: 'This is Title', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, molestias doloribus eius nesciunt, aspernatur a, perferendis suscipit laborum distinctio ipsam assumenda ipsa esse fugiat quam officia omnis saepe laboriosam autem.' },
-		{ id: uuid++, title: 'This is Title',description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, molestias doloribus eius nesciunt, aspernatur a, perferendis suscipit laborum distinctio ipsam assumenda ipsa esse fugiat quam officia omnis saepe laboriosam autem." }
-	];
-
-	function add() {
-		if (inputTitle ===""&& inputDescription === "") {
-			err = "Can't Save Empty Value!";
-		} else if (inputTitle==="") {
-			err = "Can't Save Empty Title!";
-		} else if (inputDescription==="") {
-			err = "Can't Save Empty Description!";
-		} else {
-			const newTodo ={
-				id : uuid++,
-				title: inputTitle ,
-				description : inputDescription
-			}
-			console.log(newTodo)
-			todos =[...todos,newTodo]
+	const deleteSubmit = ()=>{
+		return async ({result,update})=>{
+			todos = todos.filter(todo=>{console.log(todo.id !== result.data.id)})
+			await update()
 		}
 	}
 
-	function remove(todo){
-		todos = todos.filter(t=>t!==todo)
-	}
+
 </script>
 
 <div class="flex flex-col gap-5 p-10 font-mono ml-20">
 	<p>Make some Tasks</p>
-	<label for="title">Title</label>
-	<input
-		id="title"
-		type="text"
-		class="w-60 border-4 transition-all ease-linear focus:w-64 focus:solid-shadow border-yellow-300 outline-none"
-		bind:value={inputTitle}
-	/>
-	<label for="description">Description</label>
-	<input
-		id="description"
-		type="text"
-		class="w-60 border-4 transition-all ease-linear focus:w-64 focus:solid-shadow border-yellow-300 outline-none"
-		bind:value={inputDescription}
-	/>
-	<button
-		class="bg-blue-600 text-white w-20 transition-all ease-out duration-300 hover:text-black hover:solid-shadow active:bg-blue-800"
-		on:click={() => add()}
-	>
-		Add Task</button
-	>
-	{#if err}
+	<form method="POST" action="?/add" class="flex flex-col gap-4" use:enhance={formSubmit}>
+		<label for="title" class="inline-block w-6">Title</label>
+		<input
+			id="title"
+			name="title"
+			type="text"
+			class="w-60 border-4 transition-all ease-linear focus:w-64 focus:solid-shadow border-yellow-300 outline-none"
+		/>
+		<label for="description" class="inline-block w-20">Description</label>
+		<input
+			id="description"
+			type="text"
+			name="description"
+			class="w-60 border-4 transition-all ease-linear focus:w-64 focus:solid-shadow border-yellow-300 outline-none"
+		/>
+		<button
+			class="bg-blue-600 text-white w-20 transition-all ease-out duration-300 hover:text-black hover:solid-shadow active:bg-blue-800"
+			type="submit"
+		>
+			Add Task</button
+		>
+	</form>
+	{#if form?.err}
 		<span class="text-red-600 font-bold">
-			{err}
+			{form?.err}
 		</span>
 	{/if}
 	<div class="flex flex-wrap gap-20 mt-2">
-		{#each todos as todo}
-		<div
-			id={todo.id}
-			class="relative h-80 w-80 p-4 flex flex-col gap-4 text-white bg-blue-500 transition-all ease-in hover:text-black hover:bg-blue-600 hover:solid-shadow-xl hidden-scroll overflow-hidden hover:overflow-y-scroll hover:overflow-x-hidden"
-		>
-			<p class="text-xl">{todo.title}</p>
-			<button class="w-6 h-6 font-semibold text-sm bg-red-600 absolute right-2 top-2" on:click={()=>{remove(todo)}}> X </button>
-			<p class="text-lg leading-relaxed">
-				{todo.description}
-			</p>
-		</div>
-		{/each}
-		
+		{#if todos.length === 0}
+			<p>No Task</p>
+		{:else}
+			{#each todos as todo}
+				<div
+					class="relative h-80 w-80 p-4 flex flex-col gap-4 text-white bg-blue-500 transition-all ease-in hover:text-black hover:bg-blue-600 hover:solid-shadow-xl hidden-scroll overflow-hidden hover:overflow-y-scroll hover:overflow-x-hidden"
+				>
+					<p class="text-xl">{todo.title}</p>
+					<form method="post" action="?/delete" class="absolute right-2 top-2" use:enhance={deleteSubmit}>
+						<input type="text" name="id" value={todo.id} class="hidden">
+					    <button type="submit" placeholder="X" class="w-6 h-6 font-semibold text-sm bg-red-600">
+						X
+					</button>
+					</form>
+					<p class="text-lg leading-relaxed">
+						{todo.description}
+					</p>
+				</div>
+			{/each}
+		{/if}
 	</div>
 </div>
